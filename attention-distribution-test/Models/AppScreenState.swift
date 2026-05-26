220c7b2b1a2bcd7c042ea321daacf5bc128ae057
Taiyo Koshiba <solluna.1119@gmail.com>
2026-05-26 18:37:52 +0900

    状態管理の整理

---
+++ attention-distribution-test/Models/AppScreenState.swift
@@ -0,0 +1,16 @@
+//
+//  AppScreenState.swift
+//  attention-distribution-test
+//
+//  Created by Taiyo KOSHIBA on 2025/08/18.
+//
+
+import Foundation
+
+// アプリの画面遷移を管理する列挙型
+enum AppScreenState {
+    case start
+    case countdown
+    case test
+    case result
+}
